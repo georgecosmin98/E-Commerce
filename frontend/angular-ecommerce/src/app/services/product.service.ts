a25a5427e04a4d45c273dc12525af3d0d3f25ae8
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2021-10-06 13:15:39 +0300

    Update ProductService to call URL on Spring Boot app

--- frontend/angular-ecommerce/src/app/services/product.service.ts
+++ frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -11,8 +11,8 @@ import { ProductCategory } from '../common/product-category';
 export class ProductService {
 
   private baseUrl = 'http://localhost:8080/api/products';
-
   private categoryUrl = 'http://localhost:8080/api/product-category';
+
   constructor(private httpClient: HttpClient) { }
 
   getProductList(theCategoryId: number): Observable<Product[]> {
@@ -20,9 +20,7 @@ export class ProductService {
     // need to build URL based on category id ...
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
 
-    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
-      map(response => response._embedded.products)
-    );
+    return this.getProducts(searchUrl);
   }
 
   getProductCategories(): Observable<ProductCategory[]> {
@@ -32,8 +30,17 @@ export class ProductService {
     );
   }
 
-  searchProducts(theKeyword: string) {
-    throw new Error('Method not implemented.');
+  searchProducts(theKeyword: string): Observable<Product[]> {
+    // need to build URL based on category id ...
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`
+
+    return this.getProducts(searchUrl);
+  }
+
+  private getProducts(searchUrl: string): Observable<Product[]> {
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
+      map(response => response._embedded.products)
+    );
   }
 }
 
