14d1f0613b08fdb8e1e8a51506eaee09142340b8
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2021-10-07 16:36:20 +0300

    Refactor Cart Service

--- frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -24,12 +24,8 @@ export class CartService {
 
       // find the item in the cart based on id
 
-      for (let tempCartItem of this.cartItems) {
-        if (tempCartItem.id === theCartItem.id) {
-          existingCartItem = tempCartItem;
-          break;
-        }
-      }
+      existingCartItem = this.cartItems.find(tempCartItem => tempCartItem.id === theCartItem.id);
+
       // check if we found it
       alreadyExistsInCart = (existingCartItem != undefined)
     }
