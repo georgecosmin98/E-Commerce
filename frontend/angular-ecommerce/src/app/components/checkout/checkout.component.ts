2d408f32908190e29596af0f041f939da7255eef
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2021-10-08 15:31:25 +0300

    Add Billing Address into Checkout Form Layout

--- frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -50,4 +50,14 @@ export class CheckoutComponent implements OnInit {
     console.log("Handling the submit button")
     console.log(this.checkoutFormGroup.get('customer').value)
   }
+
+  copyShippingAddressToBillingAddress(event: any) {
+    if (event.target.checked) {
+      this.checkoutFormGroup.controls.billingAddress.setValue(
+        this.checkoutFormGroup.controls.shippingAddress.value);
+    }
+    else {
+      this.checkoutFormGroup.controls.billingAddress.reset();
+    }
+  }
 }
