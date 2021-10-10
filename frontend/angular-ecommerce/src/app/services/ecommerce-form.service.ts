e81100933afc00dcd4fda48ad992b71d49085fdc
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2021-10-10 13:39:43 +0300

    Create Form Service

---
+++ frontend/angular-ecommerce/src/app/services/ecommerce-form.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EcommerceFormService {
+
+  constructor() { }
+
+  getCreditCardMonths(startMonth: number): Observable<number[]> {
+
+    let data: number[] = [];
+
+    // build an array oy "Month" dropdown list
+    // - start at current month and loop until
+
+    for (let theMonth = startMonth; theMonth <= 12; theMonth++) {
+      data.push(theMonth);
+    }
+
+    // wrap "data" object as an Observable
+    return of(data);
+  }
+
+  getCreditCardYears(): Observable<number[]> {
+
+    let data: number[] = [];
+
+    // build an array for "Year" dropdown list
+    // -start at current year and loop over next 10 years
+
+    const startYear: number = new Date().getFullYear();
+
+    const endYear: number = startYear + 10;
+
+    for (let theYear = startYear; theYear <= endYear; theYear++) {
+      data.push(theYear);
+    }
+
+    // wrap "data" object as an Observable
+    return of(data)
+
+  }
+}
