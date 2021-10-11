3cc489587987a1fba4600b73ca85fa0b802cc08d
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2021-10-11 12:01:33 +0300

    Read the selected country, retrieve list of states based on selected country

--- frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Country } from 'src/app/common/country';
+import { State } from 'src/app/common/state';
 import { EcommerceFormService } from 'src/app/services/ecommerce-form.service';
 
 @Component({
@@ -20,6 +21,8 @@ export class CheckoutComponent implements OnInit {
   creditCardMonths: number[] = [];
 
   countries: Country[] = [];
+  shippingAddressStates: State[] = [];
+  billingAddressStates: State[] = [];
 
   constructor(private formBuilder: FormBuilder,
     private ecommerceFormService: EcommerceFormService) { }
@@ -120,6 +123,29 @@ export class CheckoutComponent implements OnInit {
         this.creditCardMonths = data;
       }
     )
+  }
 
+  getStates(FormGroupName: string) {
+
+    const formGroup = this.checkoutFormGroup.get(FormGroupName);
+
+    const countryCode = formGroup.value.country.code;
+    const countryName = formGroup.value.country.name;
+
+    console.log(`{formGroupName} country code: ${countryCode}`)
+    console.log(`{FormGroupName} country code: ${countryName}`)
+
+    this.ecommerceFormService.getStates(countryCode).subscribe(
+      data => {
+        if (FormGroupName === "shippingAddress") {
+          this.shippingAddressStates = data;
+        } else {
+          this.billingAddressStates = data;
+        }
+
+        // select first item by default
+        formGroup.get('state').setValue(data[0]);
+      }
+    )
   }
 }
