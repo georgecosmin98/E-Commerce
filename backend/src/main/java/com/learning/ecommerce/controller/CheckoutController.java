7fdf0a3a6682fa5a3a76e269e76ca5ece6fa9fcd
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2021-10-12 11:30:38 +0300

    Create controller class for checkout

---
+++ backend/src/main/java/com/learning/ecommerce/controller/CheckoutController.java
@@ -0,0 +1,25 @@
+package com.learning.ecommerce.controller;
+
+import com.learning.ecommerce.dto.Purchase;
+import com.learning.ecommerce.dto.PurchaseResponse;
+import com.learning.ecommerce.service.CheckoutService;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:4200")
+@RestController
+@RequestMapping("/api/checkout")
+public class CheckoutController {
+
+    private CheckoutService checkoutService;
+
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+
+        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+        return purchaseResponse;
+    }
+}
