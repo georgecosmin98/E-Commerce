f9d99b34cca64661284c70cd23efb69e2890c99f
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2021-10-14 16:20:27 +0300

    Expose just repository annotated with @RepositoryRestResource

--- backend/src/main/java/com/learning/ecommerce/dao/ProductRepository.java
+++ backend/src/main/java/com/learning/ecommerce/dao/ProductRepository.java
@@ -4,7 +4,6 @@ import com.learning.ecommerce.entity.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
