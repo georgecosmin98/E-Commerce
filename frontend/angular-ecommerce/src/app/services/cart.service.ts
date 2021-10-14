830f960b29b1d74d576b029c6b82788c1fd480ce
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2021-10-14 14:36:42 +0300

    Update CartService to read data from session storage and store into it

--- frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -12,7 +12,20 @@ export class CartService {
   totalPrice: Subject<number> = new BehaviorSubject<number>(0);
   totalQuantity: Subject<number> = new BehaviorSubject<number>(0);
 
-  constructor() { }
+  storage: Storage = sessionStorage;
+
+  constructor() {
+
+    // read data from storage
+    let data = JSON.parse(this.storage.getItem('cartItems'));
+
+    if (data != null) {
+      this.cartItems = data;
+    }
+
+    // compute totals based on the data that is read from storage
+    this.computeCartTotals();
+  }
 
   addToCart(theCartItem: CartItem) {
 
@@ -59,6 +72,10 @@ export class CartService {
 
     // log cart data just for debugging purpose
     this.logCartData(totalPriceValue, totalQuantityValue);
+  }
+
+  persistCartItems() {
+    this.storage.setItem('cartItems', JSON.stringify(this.cartItems))
   }
 
   logCartData(totalPriceValue: number, totalQuantityValue: number) {
