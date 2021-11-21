4bf0af510c510c3c886aa491019a9a821f90288e
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2021-11-21 12:39:11 +0200

    Fix different product box size

--- frontend/angular-ecommerce/src/app/services/auth-interceptor.service.ts
+++ frontend/angular-ecommerce/src/app/services/auth-interceptor.service.ts
@@ -1,7 +1,6 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { OktaAuthService } from '@okta/okta-angular';
-import { request } from 'http';
 import { from, Observable } from 'rxjs';
 
 @Injectable({
